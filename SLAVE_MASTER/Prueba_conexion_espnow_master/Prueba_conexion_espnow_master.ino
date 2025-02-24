a7ed91a276c280c4e5f7421cf5bb631754ae0242
G0ATEN <adriandiazgomez2014@gmail.com>
2025-02-24 21:32:36 +0100

    Update Prueba_conexion_espnow_master.ino
    Se ha cambiado el código del master para que use la UART1 (la de los pines)

--- SLAVE_MASTER/Prueba_conexion_espnow_master/Prueba_conexion_espnow_master.ino
+++ SLAVE_MASTER/Prueba_conexion_espnow_master/Prueba_conexion_espnow_master.ino
@@ -1,6 +1,12 @@
 #include <WiFi.h>
 #include <esp_now.h>
+#include <HardwareSerialUART.h>
 
+
+#define TX_PIN 6  // (TX)
+#define RX_PIN 7  // (RX)
+// UART1 (para los pines)
+HardwareSerial SerialUART(1); 
 
 // Prueba para el master, la mac destino es broadcast
 uint8_t macDestino[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
@@ -15,29 +21,29 @@ typedef struct struct_message {
 
 // Configuro lo que pasa cuando se recibe
 void receiveDATA(const esp_now_recv_info_t *info, const uint8_t *incomingData, int len) {
-  // Serial.print("HOLA");
+  // SerialUART.print("HOLA");
   struct_message datos_recibidos;
   memcpy(&datos_recibidos, incomingData, sizeof(datos_recibidos));
-  Serial.print("MAC: ");
+  SerialUART.print("MAC: ");
   for (int i = 0; i < 6; i++) {
-      Serial.printf("%02X", info->src_addr[i]);
-      if (i < 5) Serial.print(":");
+      SerialUART.printf("%02X", info->src_addr[i]);
+      if (i < 5) SerialUART.print(":");
   }
-  Serial.println();
-  Serial.print("T: ");
-  Serial.println(datos_recibidos.temperatura);
-  Serial.print("H: ");
-  Serial.println(datos_recibidos.humedad);
-  Serial.print("p: ");
-  Serial.println(datos_recibidos.presion); 
-  Serial.print("EXT: ");
-  Serial.println(datos_recibidos.exterior);
+  SerialUART.println();
+  SerialUART.print("T: ");
+  SerialUART.println(datos_recibidos.temperatura);
+  SerialUART.print("H: ");
+  SerialUART.println(datos_recibidos.humedad);
+  SerialUART.print("p: ");
+  SerialUART.println(datos_recibidos.presion); 
+  SerialUART.print("EXT: ");
+  SerialUART.println(datos_recibidos.exterior);
 }
 void setup() {
-  Serial.begin(115200);
+  SerialUART.begin(9600, SERIAL_8N1, RX_PIN, TX_PIN);
   WiFi.mode(WIFI_STA); // Configurar Wi-Fi en modo estación
   if (esp_now_init() != ESP_OK) {
-    Serial.println("Error al inicializar ESP-NOW");
+    SerialUART.println("Error al inicializar ESP-NOW");
   }
   // Recepción de medidas
   esp_now_register_recv_cb(receiveDATA);
@@ -47,7 +53,7 @@ void setup() {
   peerInfo.channel = 0;
   peerInfo.encrypt = false;
   if (esp_now_add_peer(&peerInfo) != ESP_OK) {
-    Serial.println("⚠ Error al agregar peer de Broadcast");
+    SerialUART.println("⚠ Error al agregar peer de Broadcast");
     return;
   }
 }
@@ -60,7 +66,7 @@ void loop() {
           &datos[0], &datos[1], &datos[2], 
           &datos[3], &datos[4], &datos[5]);
   esp_err_t result = esp_now_send(macDestino, datos, sizeof(datos));
-  // Serial.print("Estado del envío: ");
-  // Serial.println(result == ESP_OK ? "✔ Éxito" : "❌ Falló");
+  // SerialUART.print("Estado del envío: ");
+  // SerialUART.println(result == ESP_OK ? "✔ Éxito" : "❌ Falló");
   delay(100);
 }
